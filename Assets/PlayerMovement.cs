df5753289bb6b77b7ecdac93d51ed0f49ebc49ae
Lewis Johnson <lewis.johnson@greenteagames.com>
2019-11-21 09:58:21 +0000

    init commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float MovementSpeed;
+	Rigidbody2D rb;
+
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();    
+    }
+	
+    void Update()
+    {
+		//Movement
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * MovementSpeed, Input.GetAxis("Vertical") * MovementSpeed);
+
+		#region Bounds Section
+		//Bounds [This is to prevent the player from breaching the bounds of the map.]
+		if (transform.position.x > 450)
+			transform.position = new Vector3(450, transform.position.y);
+
+		if (transform.position.x < -450)
+			transform.position = new Vector3(-450, transform.position.y);
+
+		if (transform.position.y > 280)
+			transform.position = new Vector3(transform.position.x, 280);
+
+		if (transform.position.y < -280)
+			transform.position = new Vector3(transform.position.x, -280);
+		#endregion
+	}
+}
---
+++ Assets/SmoothFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+	[SerializeField] Transform Target;
+	[SerializeField] float followSpeed;
+
+    void FixedUpdate()
+    {
+		Vector3 MoveToPos = new Vector3(Target.position.x, Target.position.y, -10);
+
+		transform.position = Vector3.Lerp(transform.position, MoveToPos, followSpeed * Time.deltaTime);
+    }
+}
